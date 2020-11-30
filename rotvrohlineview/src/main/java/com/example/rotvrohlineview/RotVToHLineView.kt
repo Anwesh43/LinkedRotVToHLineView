1a6c36125366e5de9199185ea4fbf20bfe321776
Anwesh43 <anweshthecool0@gmail.com>
2020-11-30 13:10:46 +0530

    adding static create method

--- rotvrohlineview/src/main/java/com/example/rotvrohlineview/RotVToHLineView.kt
+++ rotvrohlineview/src/main/java/com/example/rotvrohlineview/RotVToHLineView.kt
@@ -213,4 +213,13 @@ class RotVToHLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : RotVToHLineView {
+            val view : RotVToHLineView = RotVToHLineView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
