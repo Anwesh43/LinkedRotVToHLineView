861557e1b4958dd34fddce149af6901406dfac71
Anwesh43 <anweshthecool0@gmail.com>
2020-11-30 12:32:16 +0530

    adding constants and libraries

--- rotvrohlineview/src/main/java/com/example/rotvrohlineview/RotVToHLineView.kt
+++ rotvrohlineview/src/main/java/com/example/rotvrohlineview/RotVToHLineView.kt
@@ -1,2 +1,26 @@
 package com.example.rotvrohlineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#673AB7",
+    "#4CAF50",
+    "#2196F3",
+    "#FFC107"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.6f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+
